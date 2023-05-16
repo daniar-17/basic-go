e0b3768cdb2bb5a54ed02764f5c49eaafb2d1a6c
daniar-17 <daniarnuramin1@gmail.com>
2023-05-16 10:53:15 +0700

    Update Slice Map Golang

--- map.go
+++ map.go
@@ -13,4 +13,40 @@ func main()  {
 	myMap["JavaScript"] = 6
 	fmt.Println("All Result : ", myMap)
 	fmt.Println("Spesifc Map : ",myMap["Php"])
+
+	//Declare Map
+	secondMap := map[string]string{
+		"Apple" : "is Apple",
+		"Samsung" : "is Samsung",
+		"Xiaomi" : "is Xiaomi",
+	}
+	fmt.Println("Looping with For")
+	for key, value := range secondMap {
+		fmt.Println("Key :", key,", Value :", value)
+	}
+
+	//Add Map
+	secondMap["Asus"] = "is Asus"
+	fmt.Println(secondMap)
+
+	//Delete Map
+	delete(secondMap, "Xiaomi")
+	fmt.Println(secondMap)
+
+	//Check Value Map
+	value, isAvailable := secondMap["Nokia"]
+	fmt.Println(value)
+	fmt.Println(isAvailable)
+
+	//Slice Map
+	students := []map[string]string{
+		{"name": "Agung", "Score":"A", "Study":"Math"},
+		{"name": "Sarif", "Score":"B", "Study":"Algoritma"},
+		{"name": "Agung", "Score":"C", "Study":"English"},
+	}
+
+	for _, student := range students{
+		fmt.Println(student)
+		fmt.Println("Name : ",student["name"], "-", "Study : ", student["Study"])
+	}
 }
